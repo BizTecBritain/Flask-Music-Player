c57d22b7b44d0fe3c1e5938c99aa6d7a63ee72c1
BizTecBritain <81611954+BizTecBritain@users.noreply.github.com>
2021-04-01 17:21:45 +0100

    Delete pySource directory

--- pySource/app/main/extras.py
+++
@@ -1,7 +0,0 @@
-from flask import send_from_directory
-from . import main
-import os
-
-@main.route('/images/<path:filename>')
-def download_file(filename):
-    return send_from_directory("../../images/", filename, as_attachment=True)
